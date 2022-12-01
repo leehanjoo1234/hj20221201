b160270b33dc1ce4084ec7a672eca81c83b317cb
82102 <82102@DESKTOP-LOCUK6S>
2022-12-01 12:28:23 +0900

    이한주_프로젝트 초기설정

---
+++ hj20221201/src/main/java/com/oracle/hj20221201/Hj20221201Application.java
@@ -0,0 +1,13 @@
+package com.oracle.hj20221201;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hj20221201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hj20221201Application.class, args);
+	}
+
+}
---
+++ hj20221201/src/test/java/com/oracle/hj20221201/Hj20221201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.hj20221201;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hj20221201ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
